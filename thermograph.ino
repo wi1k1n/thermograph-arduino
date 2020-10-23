87ab8bb8cd911331231a21a7b174c854c61511f3
Ilia Mazlov <mazlov.i.a@gmail.com>
2020-10-23 17:09:56 +0200

    refactor;

--- thermograph.ino
+++ thermograph.ino
@@ -8,11 +8,11 @@
 #include "thermistorMinim.h"
 
 
-// Settings constants
-#define TEMPSHOWINTERVAL 500
-#define TEMPSTOREINTERVALDEFAULT 1000
+// Main constants
 #define TEMPAVERAGEN 20  // [0..255] - number of intermediate measurements
 #define MEASDATALENGTH 128  // bigger than SCREEN_WIDTH is not supported yet
+#define GRAPHBTNSTEPS4SPEED 15  // steps after which speed is increased by GRAPHBTNSPEED
+#define GRAPHBTNSPEED 2
 
 //#define EEPROMMEASBITS 5  // [1..8] - how many bits to use for each measurement when storing in EEPROM
 //#define EEPROMMEASRANGE 32  // [2^EEPROMMEASBITS .. 256] - what range of temperatures (in degrees of celsium) each measurement covers
@@ -27,22 +27,25 @@
 #define BTNRIGHT_PIN 2
 #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define BTNDEBOUNCE 40
+
+// Timeouts
+#define TEMPSHOWINTERVAL 500
+#define TEMPSTOREINTERVALDEFAULT 1000
+#define EEPROMSAVEMENUTIMEOUT 5000
 #define BTNCLICKTIMEOUT 150  // timeout between consequent clicks
 #define BTNHOLDTIMEOUT 400  // timeout for holding a button
 #define BTNSTEPTIMEOUT 80  // timout between steps while holding button
 
-#define GRAPHBTNSTEPS4SPEED 15  // steps after which speed is increased by 2
-
+// Display constants
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define DISPLAYPADDINGTOP 16
-#define DISPLAYDATAHEIGHT 64-16  // SCREEN_HEIGHT - DISPLAYPADDINGTOP
+#define DISPLAYDATAHEIGHT SCREEN_HEIGHT - DISPLAYPADDINGTOP  // SCREEN_HEIGHT - DISPLAYPADDINGTOP
 // #define GRAPHYOFFSET (64 - 16) / 2  // DISPLAYDATAHEIGHT / 2
 #define DISPLAYLOGODURATION 1000
-#define GRAPHPADDINGTOP 2
+#define GRAPHPADDINGTOP 2  // extra padding for graph area (to make graph cursor visually more recognizable)
 #define GRAPHPADDINGBOT 2
 
-#define EEPROMSAVEMENUTIMEOUT 5000
 
 #define PGM_READ_CHARARR(val) (char*)pgm_read_word(&val)
 
@@ -264,7 +267,7 @@ void loop() {
         // if graphCursor mode is active
         if (graphCurs < MEASDATALENGTH) {
           // Serial.println(btnL.getHoldClicks());
-          graphCursorMove(++graphCursSteps < GRAPHBTNSTEPS4SPEED ? -1 : -2);
+          graphCursorMove(++graphCursSteps < GRAPHBTNSTEPS4SPEED ? -1 : -GRAPHBTNSPEED);
           forceMenuRedraw = true;
         }
       }
@@ -277,7 +280,7 @@ void loop() {
       if (menuScreen == 1) {
         // if graphCursor mode is active
         if (graphCurs < MEASDATALENGTH) {
-          graphCursorMove(++graphCursSteps < GRAPHBTNSTEPS4SPEED ? 1 : 2);
+          graphCursorMove(++graphCursSteps < GRAPHBTNSTEPS4SPEED ? 1 : GRAPHBTNSPEED);
           forceMenuRedraw = true;
         }
       }
